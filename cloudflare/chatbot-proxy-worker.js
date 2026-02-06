61f0df11101a0a8648f82ac251e0c3d720ba9e71
Muhammad Hasaan Chaudhry <muhammadhasaan82@gmail.com>
2026-02-07 00:01:48 +0500

    Add CORS handling to Cloudflare Worker proxy

--- cloudflare/chatbot-proxy-worker.js
+++ cloudflare/chatbot-proxy-worker.js
@@ -1,5 +1,25 @@
 export default {
   async fetch(request) {
+    const allowedOrigins = [
+      'https://nexgenteck.github.io',
+      'https://muhammadhasaan82.github.io',
+    ];
+
+    const origin = request.headers.get('Origin');
+    const allowOrigin = allowedOrigins.includes(origin) ? origin : allowedOrigins[0];
+
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          'Access-Control-Allow-Origin': allowOrigin,
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Max-Age': '86400',
+        },
+      });
+    }
+
     const url = new URL(request.url);
     const target = `http://165.245.177.103${url.pathname}${url.search}`;
 
@@ -11,10 +31,15 @@ export default {
     };
 
     const response = await fetch(target, init);
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set('Access-Control-Allow-Origin', allowOrigin);
+    newHeaders.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    newHeaders.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
-      headers: response.headers,
+      headers: newHeaders,
     });
   },
 };
